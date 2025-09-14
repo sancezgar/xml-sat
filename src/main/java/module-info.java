eae30c8fca56261fdf933020b18176c5abc181cb
Uriel Sanchez <sancezgar@gmail.com>
2025-09-14 12:56:29 -0600

    Pruebas con el DirectoryChooser para obtener el listado de xml.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.usanchez.procesa_xml_sat {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens org.usanchez.procesa_xml_sat to javafx.fxml;
     exports org.usanchez.procesa_xml_sat;
+    opens org.usanchez.procesa_xml_sat.controller to javafx.fxml;
+    exports org.usanchez.procesa_xml_sat.controller;
 }
--- src/main/java/org/usanchez/procesa_xml_sat/controller/AppController.java
+++ src/main/java/org/usanchez/procesa_xml_sat/controller/AppController.java
@@ -1,4 +1,33 @@
 package org.usanchez.procesa_xml_sat.controller;
 
-public class AppController {
+import javafx.fxml.Initializable;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+import org.usanchez.procesa_xml_sat.App;
+
+import javax.swing.*;
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AppController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public String importar(){
+        String ruta="";
+        DirectoryChooser dc = new DirectoryChooser();
+        File selectedDirectory =  dc.showDialog(null);
+        if(selectedDirectory != null){
+            ruta =  selectedDirectory.getAbsolutePath();
+            System.out.println("ruta: " + ruta);
+        }else{
+            System.out.println("No se escogio la ruta.");
+        }
+        return ruta;
+    }
+
 }
