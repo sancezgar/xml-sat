467780ec5932abb3e41cb98c3bc607dd3dc84974
sancezgar <sancezgar@gmail.com>
2025-09-14 18:19:49 -0600

    Se crea Rama

--- src/main/java/org/usanchez/procesa_xml_sat/App.java
+++ src/main/java/org/usanchez/procesa_xml_sat/App.java
@@ -4,23 +4,45 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class App extends Application {
+
+    private double x=0;
+    private double y=0;
+
     @Override
     public void start(Stage stage) throws IOException {
         showScreen("app");
     }
 
-    public static void showScreen(String fxml) throws IOException {
+    public void showScreen(String fxml) throws IOException {
         URL url = App.class.getResource(fxml + ".fxml");
         Parent root = FXMLLoader.load(url);
         Stage stage = new Stage();
         Scene scene = new Scene(root);
-        //stage.setMaximized(true);
+
+        root.setOnMousePressed((MouseEvent event) ->{
+            x = event.getSceneX();
+            y = event.getSceneY();
+            stage.setOpacity(0.8);
+        });
+
+        root.setOnMouseReleased((MouseEvent event)->{
+            stage.setOpacity(1);
+        });
+
+        root.setOnMouseDragged((MouseEvent event) ->{
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/usanchez/procesa_xml_sat/controller/AppController.java
+++ src/main/java/org/usanchez/procesa_xml_sat/controller/AppController.java
@@ -1,16 +1,24 @@
 package org.usanchez.procesa_xml_sat.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.usanchez.procesa_xml_sat.App;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AppController implements Initializable {
+
+    @FXML
+    private AnchorPane appWindow;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -21,6 +29,7 @@ public class AppController implements Initializable {
         String ruta="";
         DirectoryChooser dc = new DirectoryChooser();
         File selectedDirectory =  dc.showDialog(null);
+        dc.setInitialDirectory(new File(System.getProperty("user.home")));
         if(selectedDirectory != null){
             ruta =  selectedDirectory.getAbsolutePath();
             System.out.println("ruta: " + ruta);
@@ -28,6 +37,28 @@ public class AppController implements Initializable {
             System.out.println("No se escogio la ruta.");
         }
         return ruta;
+    }
+
+    public void minimizar(){
+        Stage stage = (Stage) appWindow.getScene().getWindow();
+
+        //Minimizar la ventana
+        stage.setIconified(true);
+
+    }
+
+    public void maximizar(){
+        Stage stage = (Stage) appWindow.getScene().getWindow();
+
+        if(stage.isMaximized()){
+            stage.setMaximized(false);
+        }else{
+            stage.setMaximized(true);
+        }
+    }
+
+    public void cerrarVentana(){
+        System.exit(0);
     }
 
 }
